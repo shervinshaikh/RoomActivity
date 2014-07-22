99520cb5a239400be6b40c8cdd3cf157f4738040
Shervin Shaikh <shervinater@gmail.com>
2014-07-22 15:17:44 -0700

    Seems to be working, time to deploy

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('conferenceApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, $interval) {
     $scope.room1 = "available";
     // $scope.room2 = "available";
 
-
+    var THRESHOLD = 100000;
 
     // Motion sensor 1
     $http({
@@ -17,7 +17,7 @@ angular.module('conferenceApp')
       }
     })
     .success(function(data, status, headers, config) {
-      console.log(data.data);
+      // console.log(data.data);
       $scope.motion1Data = data.data;
       $scope.motion1 = (data.data[0].motion === "active") ? true : false;
 
@@ -49,11 +49,11 @@ angular.module('conferenceApp')
       }
     })
     .success(function(data, status, headers, config) {
-      console.log(data.data);
+      // console.log(data.data);
       $scope.motion2Data = data.data;
 
-      var diff = $scope.timeDiff(data);
-      $scope.motion2d = (diff < 100000) ? true : false;
+      var diff = $scope.timeDiff(data.data);
+      $scope.motion2d = (diff < THRESHOLD) ? true : false;
 
       $scope.motion2 = (data.data[0].motion === "active") ? true : false;
       $scope.room1 = ($scope.motion1 && $scope.motion2d) ? "unavailable" : "available";
@@ -62,10 +62,10 @@ angular.module('conferenceApp')
 
     $scope.timeDiff = function(data) {
       var diff = 0;
-      for(var i=0; i<data.data.length; i++){
-        console.log(data.data[i].motion);
-        var t = Date.parse(data.data[i].timestamp);
-        console.log(t);
+      for(var i=0; i<data.length; i++){
+        // console.log(data[i].motion);
+        var t = Date.parse(data[i].timestamp);
+        // console.log(t);
         if(i === 0){
           diff = t;
         } else if(i === 9){
@@ -86,7 +86,7 @@ angular.module('conferenceApp')
     });
 
     conn.on('ready', function(data){
-      console.log('Ready');
+      // console.log('Ready');
       conn.status(function (data) {
         // console.log(data);
       });
@@ -100,29 +100,46 @@ angular.module('conferenceApp')
             
 
             var dat = message.payload.dat;
-            console.log(dat);
-            console.log(Date.parse(message.timestamp));
+            // console.log(dat);
+            // console.log(Date.parse(message.timestamp));
 
             $scope.motion1 = (message.payload.dat.motion === "active") ? true : false;
             $scope.room1 = ($scope.motion1 && $scope.motion2d) ? "unavailable" : "available";
             $scope.$apply();
           } else if(message.payload.dat.num === "2") {
-            console.log("here");
-            console.log($scope.motion2Data);
+            
+            // console.log($scope.motion2Data);
             $scope.motion2Data.unshift({
               "motion": message.payload.dat.motion,
-              "timestamp": message.payload.timestamp
+              "timestamp": message.timestamp
             });
-            console.log($scope.motion2Data);
+            // console.log($scope.motion2Data);
             $scope.motion2Data.pop();
-            console.log($scope.motion2Data);
+            // console.log($scope.motion2Data);
+            // console.log("here");
+            var d = $scope.timeDiff($scope.motion2Data);
+            console.log("Diference:", d);
 
-            $scope.motion2 = (message.payload.dat.motion === "active") ? true : false;
+            $scope.motion2 = (message.payload.dat.motion2 === "active") ? true : false;
+
+
+            $scope.motion2d = (d < THRESHOLD) ? true : false;
+            $scope.room1 = ($scope.motion1 && $scope.motion2d) ? "unavailable" : "available";
             $scope.$apply();
           }
         }
       });
 
     });
+
+    $interval(function(){
+      if($scope.motion2d === true){
+        var idiff = Date.now() - Date.parse($scope.motion2Data[0].timestamp);
+        console.log("INTER Diff:", idiff);
+
+        $scope.motion2d = (idiff < 20000) ? true : false;
+        $scope.room1 = ($scope.motion1 && $scope.motion2d) ? "unavailable" : "available";
+      }
+    }, 5000);
 
   });
